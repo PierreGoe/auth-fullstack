7f3a76f0efa51867124855c7892ec117a973a484
Loic <loic.brassart@gmail.com>
2021-02-02 01:02:08 +0100

    Add front 'protection'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,43 @@
-import { Switch, Route } from "react-router";
-import { ToastContainer } from "react-toastify";
+import { Switch, Route, Redirect } from "react-router";
+import { Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import { useSelector } from 'react-redux'
 import { AdminPage, LoginPage, HomePage, Error404Page } from "./pages";
 
+const PrivateRoute = (props) => {
+  const { id } = useSelector(state => state.user);
+  const isLoggedIn = !!id;
+
+  if (isLoggedIn) {
+    return <Route {...props} />
+  } else {
+    toast.error("Could you please stop trying breaking things, sir ?")
+    return <Redirect to="/" />
+  }
+};
+
+
 function App() {
+  const { id } = useSelector(state => state.user);
+  const isLoggedIn = !!id;
+
   return (
     <div className="App">
       <header>
         <h1>Auth Workshop</h1>
         <nav>
           <ul>
-            <li>Home</li>
-            <li>Login</li>
-            <li>Admin</li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            {isLoggedIn && <li><Link to="/admin">Admin</Link></li>}
           </ul>
         </nav>
       </header>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/login" component={LoginPage} />
-        <Route path="/admin" component={AdminPage} />
+        <PrivateRoute path="/admin" component={AdminPage} />
         <Route path="/" component={Error404Page} />
       </Switch>
       <ToastContainer />
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -5,19 +5,19 @@ export function HomePage() {
       <ul>
         <li>Done: Functional backend auth (signup + login)</li>
         <li>Done: Functional frontend form to call backend auth</li>
-        <li>
-          Todo: "Protected" admin route (no link and redirection to Home for
-          anonymous users )
-        </li>
         <li>Done: 404 Error page for all undefined routes</li>
         <li>
-          Done: Centralized prepard API instance + auto-update credentials on
+          Done: Centralized prepared API instance + auto-update credentials on
           login
         </li>
         <li>Done: User data storage in Redux</li>
+        <li>
+          Todo: "Protected" admin route (no link available and auto-redirection to Home for
+          anonymous users)
+        </li>
         <li>Todo: Add links in the main menu</li>
         <li>Todo: Add styles</li>
-        <li>Todo: </li>
+        <li>Todo: Logout</li>
       </ul>
     </>
   );
